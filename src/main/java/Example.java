a5a7f1b6e49744289e9d2b1e5393a4179f2818c8
Jacob Harris <jacob.harris@e-gineering.comg>
2021-05-27 10:46:58 -0400

    Committing the example java file

---
+++ src/main/java/Example.java
@@ -0,0 +1,17 @@
+public class Example {
+    private int foo;
+    private long bar;
+    
+    public Example(int foo, int bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+    
+    public int getFoo() {
+        return foo;
+    }
+
+    public long getBar() {
+        return bar;
+    }
+}
